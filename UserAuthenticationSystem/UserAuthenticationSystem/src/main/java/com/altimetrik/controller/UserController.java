afd4f37fe0261aff44dc8c0832b3bd9ee70d45d1
monish1107 <42172671+monish1107@users.noreply.github.com>
2019-10-18 23:55:34 +0530

    Add files via upload

--- UserAuthenticationSystem/UserAuthenticationSystem/src/main/java/com/altimetrik/controller/UserController.java
+++ UserAuthenticationSystem/UserAuthenticationSystem/src/main/java/com/altimetrik/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,7 +37,7 @@ public class UserController {
 	}
 
 	@PostMapping
-	public ResponseEntity<UserEntity> createOrUpdateUser(UserEntity user) throws RecordNotFoundException {
+	public ResponseEntity<UserEntity> createOrUpdateUser(@RequestBody UserEntity user) throws RecordNotFoundException {
 		UserEntity updated = service.createOrUpdateUser(user);
 		return new ResponseEntity<UserEntity>(updated, new HttpHeaders(), HttpStatus.OK);
 	}
